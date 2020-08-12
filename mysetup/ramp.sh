27d130d3d92e3f645f606b56459ed62ed54fbb61
Gabriel Vinazza <gabriel.vinazza@gmail.com>
2020-08-12 11:16:04 -0300

    Delete ramp.sh

--- mysetup/ramp.sh
+++
@@ -1,4 +0,0 @@
-cat $1/8080 | tr '\n' ' ' | sed -f replace.sed | pdsend 8080
-cat $1/8081 | tr '\n' ' ' | sed -f replace.sed | pdsend 8081
-cat $1/8082 | tr '\n' ' ' | sed -f replace.sed | pdsend 8082
-cat $1/8083 | tr '\n' ' ' | sed -f replace.sed | pdsend 8083
